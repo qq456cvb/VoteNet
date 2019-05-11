fix a few bugs; add mAP calculation; iou calc in C++ to be fixed

evaluator.py
@@ -10,99 +10,137 @@ from tensorpack.utils import logger
 from dataset import *
 import numpy as np
 import config
+from shapely.geometry import Polygon
+from sklearn.metrics import average_precision_score
 from tensorpack.utils.stats import StatCounter
 from tensorpack.utils.utils import get_tqdm_kwargs
+type_whitelist = ('bed', 'table', 'sofa', 'chair', 'toilet', 'desk', 'dresser', 'night_stand',
+                               'bookshelf', 'bathtub')
 
 import sys
 import os
 
 
-# TODO: compute mAP
-class Evaluator(Callback):
-    def eval_epoch(self):
-        for idx in range(1000):
-            try:
-                calib = self.dataset.get_calibration(idx)
-                objects = self.dataset.get_label_objects(idx)
-                pc_upright_depth = self.dataset.get_depth(idx)
-                pc_upright_depth = pc_upright_depth[
-                                    self.rng.choice(pc_upright_depth.shape[0], config.POINT_NUM, replace=False), :]  # subsample
-                pc_upright_camera = np.zeros_like(pc_upright_depth)
-                pc_upright_camera[:, 0:3] = calib.project_upright_depth_to_upright_camera(pc_upright_depth[:, 0:3])
-                pc_upright_camera[:, 3:] = pc_upright_depth[:, 3:]
-                pc_image_coord, _ = calib.project_upright_depth_to_image(pc_upright_depth)
+def iou_3d(bbox1, bbox2):
+    '''
+    return iou of two 3d bbox
+    :param bbox1: 8 * 3
+    :param bbox2: 8 * 3
+    :return: float
+    '''
+    poly1_xz = Polygon(np.stack([bbox1[:4, 0], bbox1[:4, 2]], -1))
+    poly2_xz = Polygon(np.stack([bbox2[:4, 0], bbox2[:4, 2]], -1))
+    iou_xz = poly1_xz.intersection(poly2_xz).area / poly1_xz.union(poly2_xz).area
+    return max(iou_xz * min(bbox1[0, 1], bbox2[0, 1]) - max(bbox1[4, 1], bbox2[4, 1]), 0)
 
-                bboxes_pred, classes_pred, batch_idx = self.pred_func([pc_upright_camera[None, :, :3]])
 
-                if not objects:
+def eval_mAP(dataset, pred_func, ious):
+    gt_labels_all = {iou: {t: [] for t in type2class} for iou in ious}
+    pred_scores_all = {t: [] for t in type2class}
+    for idx in range(1, 10):
+        try:
+            calib = dataset.get_calibration(idx)
+            objects = dataset.get_label_objects(idx)
+            pc_upright_depth = dataset.get_depth(idx)
+            pc_upright_depth = pc_upright_depth[
+                                np.random.choice(pc_upright_depth.shape[0], config.POINT_NUM, replace=False), :]  # subsample
+            pc_upright_camera = np.zeros_like(pc_upright_depth)
+            pc_upright_camera[:, 0:3] = calib.project_upright_depth_to_upright_camera(pc_upright_depth[:, 0:3])
+            pc_upright_camera[:, 3:] = pc_upright_depth[:, 3:]
+            pc_image_coord, _ = calib.project_upright_depth_to_image(pc_upright_depth)
+
+            bboxes_pred, class_scores_pred, _ = pred_func(pc_upright_camera[None, :, :3])
+            # sort by confidence, high 2 low
+            sort_idx = np.argsort(-np.max(class_scores_pred, axis=-1))
+            bboxes_pred = bboxes_pred[sort_idx]
+            class_scores_pred = class_scores_pred[sort_idx]
+            class_labels_pred = np.argmax(class_scores_pred, -1)
+
+            if not objects:
+                continue
+
+            gt_bboxes = []
+            gt_classes = []
+            gt_labels = {iou: [] for iou in ious}
+            for obj_idx in range(len(objects)):
+                obj = objects[obj_idx]
+                if obj.classname not in type_whitelist:
                     continue
 
-                bboxes_xyz = []
-                bboxes_lwh = []
-                semantic_labels = []
-                heading_labels = []
-                heading_residuals = []
-                size_labels = []
-                size_residuals = []
-                for obj_idx in range(len(objects)):
-                    obj = objects[obj_idx]
-                    if obj.classname not in self.type_whitelist:
-                        continue
+                # 2D BOX: Get pts rect backprojected
+                box2d = obj.box2d
+                xmin, ymin, xmax, ymax = box2d
+                box_fov_inds = (pc_image_coord[:, 0] < xmax) & (pc_image_coord[:, 0] >= xmin) & (
+                        pc_image_coord[:, 1] < ymax) & (pc_image_coord[:, 1] >= ymin)
+                pc_in_box_fov = pc_upright_camera[box_fov_inds, :]
+                # Get frustum angle (according to center pixel in 2D BOX)
+                # 3D BOX: Get pts velo in 3d box
+                box3d_pts_2d, box3d_pts_3d = compute_box_3d(obj, calib)
+                box3d_pts_3d = calib.project_upright_depth_to_upright_camera(box3d_pts_3d)
+                _, inds = extract_pc_in_box3d(pc_in_box_fov, box3d_pts_3d)
+                # Get 3D BOX size
+                box3d_size = np.array([2 * obj.l, 2 * obj.w, 2 * obj.h])
+                box3d_center = (box3d_pts_3d[0, :] + box3d_pts_3d[6, :]) / 2
 
-                    # 2D BOX: Get pts rect backprojected
-                    box2d = obj.box2d
-                    xmin, ymin, xmax, ymax = box2d
-                    box_fov_inds = (pc_image_coord[:, 0] < xmax) & (pc_image_coord[:, 0] >= xmin) & (
-                            pc_image_coord[:, 1] < ymax) & (pc_image_coord[:, 1] >= ymin)
-                    pc_in_box_fov = pc_upright_camera[box_fov_inds, :]
-                    # Get frustum angle (according to center pixel in 2D BOX)
-                    # 3D BOX: Get pts velo in 3d box
-                    box3d_pts_2d, box3d_pts_3d = compute_box_3d(obj, calib)
-                    box3d_pts_3d = calib.project_upright_depth_to_upright_camera(box3d_pts_3d)
-                    _, inds = extract_pc_in_box3d(pc_in_box_fov, box3d_pts_3d)
-                    # Get 3D BOX size
-                    box3d_size = np.array([2 * obj.l, 2 * obj.w, 2 * obj.h])
-                    box3d_center = (box3d_pts_3d[0, :] + box3d_pts_3d[6, :]) / 2
+                # Size
+                size_class, size_residual = size2class(box3d_size, obj.classname)
+                angle_class, angle_residual = angle2class(obj.heading_angle, config.NH)
 
-                    # Size
-                    size_class, size_residual = size2class(box3d_size, obj.classname)
-                    angle_class, angle_residual = angle2class(obj.heading_angle, config.NH)
+                # Reject object with too few points
+                if len(inds) < 5:
+                    continue
 
-                    # Reject object with too few points
-                    if len(inds) < 5:
-                        continue
+                bbox = get_3d_box(class2size(size_class, size_residual),
+                           class2angle(angle_class, angle_residual, config.NH), box3d_center)
+                gt_bboxes.append(bbox)
+                gt_classes.append(type2class[obj.classname])
 
-                    bboxes_xyz.append(box3d_center)
-                    bboxes_lwh.append(box3d_size)
-                    semantic_labels.append(type2class[obj.classname])
-                    heading_labels.append(angle_class)
-                    heading_residuals.append(angle_residual / (np.pi / config.NH))
-                    size_labels.append(size_class)
-                    size_residuals.append(size_residual / type_mean_size[obj.classname])
+            gt_matched = {k: False for k in range(len(gt_bboxes))}
+            for i, bbox_pred in enumerate(bboxes_pred):
+                max_overlap = -1
+                gt_match = -1
+                for j, gt_bbox in enumerate(gt_bboxes):
+                    if gt_classes[j] == class_labels_pred[i]:
+                        overlap = iou_3d(bbox_pred, gt_bbox)
+                        if overlap > max_overlap:
+                            max_overlap = overlap
+                            gt_match = j
 
-            except:
-                pass
+                for iou in ious:
+                    if max_overlap > iou and not gt_matched[gt_match]:
+                        gt_matched[gt_match] = True
+                        gt_labels[iou].append(1)
+                    else:
+                        gt_labels[iou].append(0)
 
+            if len(class_labels_pred) > 0:
+                for iou in ious:
+                    for t in type2class:
+                        cls_idx = class_labels_pred == type2class[t]
+                        pred_scores_all[t].extend(np.max(class_scores_pred, axis=-1)[cls_idx])
+                        gt_labels_all[iou][t].extend(np.asarray(gt_labels[iou])[cls_idx])
+        except Exception as e:
+            print(e)
+
+    aps = {iou: {t: 0 for t in type2class} for iou in ious}
+    for iou in ious:
+        for t in type2class:
+            aps[iou][t] = average_precision_score(gt_labels_all[iou][t], pred_scores_all[t])
+
+    return aps
+
+
+# TODO: compute mAP
+class Evaluator(Callback):
     def __init__(self, root, split, batch_size):
         self.dataset = sunrgbd_object(root, split)
-        self.batch_size = batch_size
+        self.batch_size = batch_size  # not used for now
 
     def _setup_graph(self):
-        self.pred_func = self.trainer.get_predictor(['points'], ['bboxes_pred', 'classes_pred', 'batch_idx'])
+        self.pred_func = self.trainer.get_predictor(['points'], ['bboxes_pred', 'class_scores_pred', 'batch_idx'])
 
     def _before_train(self):
-        logger.info("farmer win rate: {}".format(farmer_win_rate))
-        logger.info("lord win rate: {}".format(1 - farmer_win_rate))
-        # self.lord_win_rate.load(1 - farmer_win_rate)
-        # if t > 10 * 60:  # eval takes too long
-        #     self.eval_episode = int(self.eval_episode * 0.94)
+        print(eval_mAP(self.dataset, self.pred_func, [0.25]))
 
     def _trigger_epoch(self):
-        t = time.time()
-        farmer_win_rate = eval_with_funcs(
-            self.pred_funcs, self.eval_episode, self.get_player_fn, verbose=False)
-        t = time.time() - t
-        if t > 10 * 60:  # eval takes too long
-            self.eval_episode = int(self.eval_episode * 0.94)
-        self.trainer.monitors.put_scalar('farmer_win_rate', farmer_win_rate)
-        self.trainer.monitors.put_scalar('lord_win_rate', 1 - farmer_win_rate)
+        pass

model.py
@@ -77,40 +77,45 @@ class Model(ModelDesc):
                                                                 group_all=False, scope='proposal',
                                                                 sample_xyz=seeds_xyz)
 
+        nms_iou = tf.get_variable('nms_iou', shape=[], initializer=tf.constant_initializer(0.25), trainable=False)
         if not get_current_tower_context().is_training:
-
             def get_3d_bbox(box_size, heading_angle, center):
                 batch_size = tf.shape(heading_angle)[0]
                 c = tf.cos(heading_angle)
                 s = tf.sin(heading_angle)
-                rotation = tf.reshape(tf.stack([c, 0, s, 0, 1, 0, -s, 0, c], -1), tf.stack([batch_size, -1, 3, 3]))
-                l, w, h = box_size[..., 0], box_size[..., 1], box_size[..., 2]
+                zeros = tf.zeros_like(c)
+                ones = tf.ones_like(c)
+                rotation = tf.reshape(tf.stack([c, zeros, s, zeros, ones, zeros, -s, zeros, c], -1), tf.stack([batch_size, -1, 3, 3]))
+                l, w, h = box_size[..., 0], box_size[..., 1], box_size[..., 2]  # lwh(xzy) order!!!
                 corners = tf.reshape(tf.stack([l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2,
                                                h / 2, h / 2, h / 2, h / 2, -h / 2, -h / 2, -h / 2, -h / 2,
                                                w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2], -1),
                                      tf.stack([batch_size, -1, 3, 8]))
-                return tf.einsum('ijkl,ijlm->ijmk', rotation, corners)  # B * N * 8 * 3
+                return tf.einsum('ijkl,ijlm->ijmk', rotation, corners) + tf.expand_dims(center, 2)  # B * N * 8 * 3
 
             class_mean_size_tf = tf.constant(class_mean_size)
             size_cls_pred = tf.argmax(proposals_output[..., 5 + 2 * config.NH: 5 + 2 * config.NH + config.NS], axis=-1)
             size_cls_pred_onehot = tf.one_hot(size_cls_pred, depth=config.NS, axis=-1)  # B * N * NS
             size_residual_pred = tf.reduce_sum(tf.expand_dims(size_cls_pred_onehot, -1)
                                                * tf.reshape(proposals_output[..., 5+2 * config.NH + config.NS:5+2 * config.NH + 4 * config.NS], (-1, config.PROPOSAL_NUM, config.NS, 3)), axis=2)
-            size_pred = tf.gather_nd(class_mean_size_tf, size_cls_pred) * (1 + size_residual_pred)  # B * N * 3: size
+            size_pred = tf.gather_nd(class_mean_size_tf, tf.expand_dims(size_cls_pred, -1)) * tf.maximum(1 + size_residual_pred, 1e-6)  # B * N * 3: size
+            # with tf.control_dependencies([tf.print(size_pred[0, 0, 2])]):
             center_pred = proposals_xyz + proposals_output[..., 2:5]  # B * N * 3
             heading_cls_pred = tf.argmax(proposals_output[..., 5:5+config.NH], axis=-1)
             heading_cls_pred_onehot = tf.one_hot(heading_cls_pred, depth=config.NH, axis=-1)
-            heading_residual_pred = tf.reduce_sum(tf.expand_dims(heading_cls_pred_onehot, -1)
+            heading_residual_pred = tf.reduce_sum(heading_cls_pred_onehot
                                                   * proposals_output[..., 5 + config.NH:5+2 * config.NH], axis=2)
-            heading_pred = tf.floormod((heading_cls_pred * 2 + heading_residual_pred) * np.pi / config.NH, 2 * np.pi)
+            heading_pred = tf.floormod((tf.cast(heading_cls_pred, tf.float32) * 2 + heading_residual_pred) * np.pi / config.NH, 2 * np.pi)
 
-            bboxes = get_3d_bbox(size_pred, heading_pred, center_pred)  # B * N * 8 * 3
-            bbox_corners = tf.concat([bboxes[:, :, 6, :], bboxes[:, :, 0, :]], axis=-1)  # B * N * 6
-            nms_iou = tf.get_variable('nms_iou', shape=[], initializer=tf.constant_initializer(0.25), trainable=False)
+            # with tf.control_dependencies([tf.print(size_residual_pred[0, :10, :]), tf.print(size_pred[0, :10, :])]):
+            bboxes = get_3d_bbox(size_pred, heading_pred, center_pred)  # B * N * 8 * 3,  lhw(xyz) order!!!
+            # with tf.control_dependencies([tf.print(bboxes[0, 0])]):
+            bbox_corners = tf.concat([bboxes[:, :, 6, :], bboxes[:, :, 0, :]], axis=-1)  # B * N * 6,  lhw(xyz) order!!!
+
             nms_idx = NMS3D(bbox_corners, tf.reduce_max(proposals_output[..., -config.NC:], axis=-1), proposals_output[..., :2], nms_iou)  # Nnms * 2
 
             bboxes_pred = tf.gather_nd(bboxes, nms_idx, name='bboxes_pred')  # Nnms * 8 * 3
-            classes_pred = tf.argmax(tf.gather_nd(proposals_output[..., -config.NC:], nms_idx), axis=-1, name='classes_pred')  # Nnms
+            class_scores_pred = tf.gather_nd(proposals_output[..., -config.NC:], nms_idx, name='class_scores_pred')  # Nnms * C
             batch_idx = tf.identity(nms_idx[:, 0], name='batch_idx')  # Nnms, this is used to identify between batches
 
             return

tf_ops/3d_nms/tf_nms3d.py
@@ -7,7 +7,7 @@ sys.path.append(BASE_DIR)
 nms3d = tf.load_op_library(os.path.join(BASE_DIR, 'tf_nms3d_so.so'))
 
 
-def NMS3D(iou_threshold, bboxes, scores, objectiveness):
+def NMS3D(bboxes, scores, objectiveness, iou_threshold):
     return nms3d.non_max_suppression3d(bboxes, scores, objectiveness, iou_threshold)
 
 
@@ -21,6 +21,7 @@ if __name__=='__main__':
         [-1, -1, -1, 1, 1, 1],
         [-1, -1, -1, 0.8, 0.8, 0.8],
     ]]).astype('float32')
+    print(bboxes.shape)
     scores = np.array([[
         0.5, 0.6
     ]]).astype('float32')